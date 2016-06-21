0d2ed0b111dfc2717f0a9402c0cf33821bcd9d46
carincon93 <carincon93@gmail.com>
2016-06-20 22:02:09 -0500

    carga

---
+++ html/html/codigo.js
@@ -0,0 +1,11 @@
+
+var nombre = prompt('Cuál es su nombre?');
+
+if (nombre == 'camilo') 
+{
+	alert('Bienvenido: '+nombre+' el aprendiz');
+} 
+else 
+{
+	alert('Bienvenido: '+nombre+' usuario');
+}
